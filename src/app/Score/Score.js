024868a433aeb950ef25412bfafadefe168f38c3
Ricardo Freitas <rfreitas@morecolab.pt>
2022-04-19 17:21:58 +0100

    Versão com endpoints de filtro para menu lateral

--- src/app/Score/Score.js
+++ src/app/Score/Score.js
@@ -1,4 +1,4 @@
-import React, { forwardRef} from "react";
+import React, { useEffect, forwardRef} from "react";
 import MaterialTable from "material-table";
 import { Link } from "react-router-dom";
 import { ProgressBar } from "react-bootstrap";
@@ -23,13 +23,13 @@ import {
 import "./Score.css";
 
 function Score({loja}) {
-/*
+
 
   useEffect(() => {
     console.log("O que entra na tabela: ", loja);
   
   }, [loja]);
-*/
+
 
   const columns = [
     {
